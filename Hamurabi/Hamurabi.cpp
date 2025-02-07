0c927834225cc4986e8808640e9f12c7e4958352
totallycisbridget <158115231+totallycisbridget@users.noreply.github.com>
2025-02-07 00:58:42 +0000

    the .vs files
    These .vs files were left out for some reason

--- Hamurabi/Hamurabi.cpp
+++ Hamurabi/Hamurabi.cpp
@@ -37,7 +37,7 @@ struct gameState
         }
     }
 
-    void loadGame(std::string Game_Save_Data) //like the saveGame but loads it instead
+    bool loadGame(std::string Game_Save_Data) //like the saveGame but loads it instead
     {
         std::ifstream file(Game_Save_Data); //opens the file to read from
         if (file.is_open())
@@ -48,10 +48,12 @@ struct gameState
                 >> year; //adds each number into the corresponding variable
             file.close();
             std::cout << "load successful" << std::endl;
+            return true;
         }
         else
         {
             std::cout << "loading unsuccessful.";
+            return false;
         }
     }
 
@@ -89,25 +91,19 @@ private:
 
 
 public:
-    void start()
-    {
-        char userLoadInput; //to store user input for if they wanna load a game
-        bool playing = true; 
-
-        std::cout << "Would you like to load a save? (y/n"
-            << std::endl;
-        std::cin >> userLoadInput;
-
-        if (userLoadInput == 'y' || userLoadInput == 'Y')
-        {
-            game.loadGame("Game_Save_Data.txt");
+    void start(bool load = false) {
+        if (load) {
+            if (!game.loadGame("Game_Save_Data.txt")) {
+                std::cout << "Starting a new game instead.\n";
+            }
         }
 
-        while (playing)
-        {
+        bool playing = true;
+        while (playing) {
             playing = gameYear();
         }
     }
+    
 
 
     bool gameYear(){
@@ -160,14 +156,23 @@ public:
             }
         } while (true);
 
+        //updating the player's various counts
         game.acresCount += userLandPurchInp; //add the amount of acres user bought
         game.bushelsCount -= (userLandPurchInp * randLandPrice); //remove the money they paid for the land
+        game.bushelsCount -= userFoodInp;
 
+
+        //feeding the population
         populationFed = userFoodInp / 20; //get the number of people fed
         if (populationFed < game.popCount) //if there are less people fed than the total population, the amount of deaths will be calculated
         {
             populationStarved = game.popCount - populationFed;
             game.popCount = populationFed;
+
+            std::cout << "You have starved "
+                << populationStarved
+                << " of the population."
+                << std::endl; //teling the player how many theyve killed
         }
         else if (populationFed > game.popCount) //if there is more food given than needed, the number of immigrants will be workd out
         {
@@ -178,17 +183,18 @@ public:
                 << " immigrants have joined our beloved city"
                 << std::endl;
         }
-        game.bushelsCount -= userFoodInp;
+        
+
 
         //farming stuff
-        for (int i = 0; i < game.popCount; i++)
+        for (int i = 0; i < game.popCount; i++) //basically goes tyhrough each of the population and works out how much they harvest
         {
             totalFertileAcres += 5 + (rand() % 6); //random number from 5 - 10
         }
 
         acresPlanted = userPlantingInp / 2; //each acre requires 2 bushels to be planted
         if (acresPlanted > game.acresCount) acresPlanted = game.acresCount; //ensure that user cannot plant more than owned
-        if (acresPlanted < totalFertileAcres) acresPlanted = totalFertileAcres; 
+         
 
         game.bushelsCount -= (acresPlanted * 2);
 
@@ -206,10 +212,9 @@ public:
 
         game.year++;
 
-        if (game.checkGameStatus()) //stop the game is the game has reached 10 years
-        {
-            return false;
-        }
+
+        if (game.checkGameStatus()) return false;//stop the game is the game has reached 10 years
+        
 
         //give the user the option to save
         std::cout << "\nWould you like to save? (y/n/q yes, no or quit): ";
@@ -232,10 +237,65 @@ public:
 
 };
 
+
+class gameMainMenu
+{
+
+public:
+    void mainMenu()
+    {
+        int menuUserChoice;
+
+        do
+        {
+            std::cout << "______------Hamurabi------______\n";
+            std::cout << "1.Start New Game\n";
+            std::cout << "2.Load Game\n";
+            std::cout << "3.How To Play\n";
+            std::cout << "4.Exit\n";
+            std::cin >> menuUserChoice;
+
+            mainGame game;
+            switch (menuUserChoice)
+            {
+            case 1:
+                game.start(false);
+                return;
+
+            case 2:
+                game.start(true);
+                return;
+
+            case 3:
+                htpInstructions();
+                break;
+            case 4:
+                std::cout << "Bye."
+                    << std::endl;
+                exit(0);
+            default:
+                std::cout << "Invalid. \nPick again.";
+
+            }
+        } while (true);
+    }
+
+
+        void htpInstructions()
+        {
+            std::cout << "As our beloved Dear Leader, you are tasked with allocating bushels of grain to our people! \nYou have been given a 10 year term to prove yourself."
+                << "\nHowever, you must be be careful as you only have so many bushels of grain to feed our people with."
+                << "\n\n\nEach person requires 20 bushels to survive throughout the year."
+                << "\nOver-allocating food will bring in immigrants who will be accounted for within the next fiscal year."
+                << "\nEach person that has been fed will be able to harvest up to 10 acres of land for the year. \nEach acre of land require 2 bushels to plant seeds for the year"
+                << "\nFinally, any remaining bushels can be used to purchase more land, if you so wish.";
+        }
+};
+
 int main()
 {
-    mainGame game; //create a game object
-    game.start();//start the game
+    gameMainMenu menu;
+    menu.mainMenu();
 
     return 0;
 }
